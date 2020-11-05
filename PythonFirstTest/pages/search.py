60fcb941b6d36e95704692081d485db6b1eb954e
pzalejski <piotrzalejski@outlook.com>
2020-11-04 22:19:24 -0600

    first commit - pz

---
+++ PythonFirstTest/pages/search.py
@@ -0,0 +1,18 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+class DuckDuckGoSearchPage():
+
+    url = 'https://www.duckduckgo.com'
+
+    SEARCH_INPUT = (By.NAME, 'q')
+
+    def __init__(self, browser):
+        self.browser = browser
+
+    def load(self):
+        self.browser.get(self.URL)
+
+    def search(self, phrase):
+        search_input = self.browser.find_element(*self.SEARCH_INPUT)
+        search_input.send_keys(phrase + Keys.RETURN)
