93cb76d724e99c7ea001d4af049330d8d26ab410
pzalejski <piotrzalejski@outlook.com>
2020-11-06 22:48:22 -0600

    created dictionary and dataframe that contains song and artist names along with timestamps - pz

--- Spotify Pipeline/main.py
+++ Spotify Pipeline/main.py
@@ -28,3 +28,28 @@ if __name__ == '__main__':
                      headers=headers)
 
     data = r.json()
+    # print(data)
+
+    song_names = []
+    artist_names = []
+    played_at_list = []
+    timestamps = []
+
+    for song in data["items"]:
+        song_names.append(song['track']['name'])
+        artist_names.append(song['track']['album']['artists'][0]['name'])
+        # played at to display in hours and minutes (ie. 06:30)
+        played_at_list.append(song["played_at"][11:16])
+        # timestamp to only have yyyy-mm-dd
+        timestamps.append(song["played_at"][0:10])
+
+    song_dict = {
+        "song_name": song_names,
+        "artist_name": artist_names,
+        "played_at": played_at_list,
+        "timestamp": timestamps
+    }
+
+    song_df = pd.DataFrame(song_dict, columns=["song_name", "artist_name", "played_at", "timestamp"])
+
+    print(song_df)
