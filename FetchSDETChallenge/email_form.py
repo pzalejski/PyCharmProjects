3cfa473b44bb693521c678c1799dfb6c28478feb
pzalejski <piotrzalejski@outlook.com>
2020-11-09 21:35:20 -0600

    added email form module, generates random first and last name as wel as creation of an email - pz

---
+++ FetchSDETChallenge/email_form.py
@@ -0,0 +1,17 @@
+import names
+import random
+
+first_name = names.get_first_name()
+last_name = names.get_last_name()
+
+
+def email():
+    email_domain = random.choice(['@gmail.com', "@yahoo.com", "@outlook.com", "@aol.com", "@live.com", "@hotmail.com", "@ymail.com"])
+    number = str(random.randrange(0,999))
+    if len(last_name) > 2:
+        username = first_name[0] + last_name[:3] + number
+    else:
+        username = first_name[0] + last_name + number
+
+    return username + email_domain
+
